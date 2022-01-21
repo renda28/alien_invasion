c3d956e9d9e243327dc60aafc1bbcf53853bd991
Renzel <renzel.david@gmail.com>
2022-01-21 21:59:57 +0000

    Create ship class and draw ship on screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 
 def run_game():
@@ -13,6 +14,9 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    # Make a ship.
+    ship = Ship(screen)
+
     # Start the main loop for the game.
     while True:
         # Watch for keyboard and mouse events.
@@ -22,6 +26,7 @@ def run_game():
 
         # Redraw the screen during each pass through the loop.
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
