57bca46e20fc949bb8588dd95d9c996f1a8cb0ac
Renzel <renzel.david@gmail.com>
2022-01-29 17:22:25 +0000

    move screen updates to game_functions module

--- alien_invasion.py
+++ alien_invasion.py
@@ -19,13 +19,7 @@ def run_game():
     # Start the main loop for the game.
     while True:
         gf.check_events()
-
-        # Redraw the screen during each pass through the loop.
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
+        gf.update_screen(ai_settings, screen, ship)
 
 
 run_game()
