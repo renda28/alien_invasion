5134cd0f151ff827dec830a9fb9ddef0fcc38207
Renzel <renzel.david@gmail.com>
2022-02-22 19:11:25 +0000

    Remove print statement

--- alien_invasion.py
+++ alien_invasion.py
@@ -30,8 +30,6 @@ def run_game():
             if bullet.rect.bottom <= 0:
                 bullets.remove(bullet)
 
-        print(len(bullets))
-
         gf.update_screen(ai_settings, screen, ship, bullets)
 
 
