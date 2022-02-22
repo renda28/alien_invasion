847ed0847214ba98f01c89dfd11edb49c0405fa2
Renzel <renzel.david@gmail.com>
2022-02-22 14:35:56 +0000

    Fire Bullets

--- game_functions.py
+++ game_functions.py
@@ -1,6 +1,7 @@
 import sys
 
 import pygame
+from bullet import Bullet
 
 
 def check_events(ai_settings, screen, ship, bullets):
@@ -10,13 +11,13 @@ def check_events(ai_settings, screen, ship, bullets):
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
 
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
 
-def check_keydown_events(event, ship):
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     # Respond to keypresses.
     if event.key == pygame.K_RIGHT:
         # Move the ship to the right.
@@ -25,6 +26,11 @@ def check_keydown_events(event, ship):
     elif event.key == pygame.K_LEFT:
         # Move the ship to the right.
         ship.moving_left = True
+
+    elif event.key == pygame.K_SPACE:
+        # Create a new bullet and add it to the bullets group.
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 
 def check_keyup_events(event, ship):
@@ -39,6 +45,11 @@ def check_keyup_events(event, ship):
 def update_screen(ai_settings, screen, ship, bullets):
     """Update images on the screen and flip to the new screen"""
     screen.fill(ai_settings.bg_color)
+
+    # Redraw all bullets behind ship and aliens
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+
     ship.blitme()
 
     # Make the most recently drawn screen visible.
