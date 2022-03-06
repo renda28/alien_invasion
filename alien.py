0bbc74edfe477a3f649e9040657358a77bf4ad9c
Renzel <renzel.david@gmail.com>
2022-03-06 14:05:56 +0000

    Moving the aliens right

--- alien.py
+++ alien.py
@@ -22,6 +22,11 @@ class Alien(Sprite):
         # Store the alien's position.
         self.x = float(self.rect.x)
 
+    def update(self):
+        """Move the alien right."""
+        self.x += self.ai_settings.alien_speed_factor
+        self.rect.x = self.x
+
     def blitme(self):
         """Draw the alien at its current position."""
         self.screen.blit(self.image, self.rect)
