2f7f05dcc96046fa36470a479355b1f7c83ce26d
Renzel <renzel.david@gmail.com>
2022-03-13 12:26:42 +0000

    add scoreboard labels

--- scoreboard.py
+++ scoreboard.py
@@ -24,6 +24,7 @@ class Scoreboard():
         """Turn the score into a rendered image."""
         rounded_score = round(self.stats.score, -1)
         score_str = "{:,}".format(rounded_score)
+        score_str = "SCORE " + score_str
         self.score_image = self.font.render(
             score_str, True, self.text_color, self.ai_settings.bg_color)
 
@@ -36,6 +37,7 @@ class Scoreboard():
         """Turn the high score into a rendered image."""
         high_score = round(self.stats.high_score, -1)
         high_score_str = "{:,}".format(high_score)
+        high_score_str = "HIGH SCORE " + high_score_str
         self.high_score_image = self.font.render(
             high_score_str, True, self.text_color, self.ai_settings.bg_color)
 
@@ -46,8 +48,9 @@ class Scoreboard():
 
     def prep_level(self):
         """Turn the level into a rendered image."""
+        level = "LEVEL " + str(self.stats.level)
         self.level_image = self.font.render(
-            str(self.stats.level), True, self.text_color, self.ai_settings.bg_color)
+            level, True, self.text_color, self.ai_settings.bg_color)
 
         # Position the level below the score.
         self.level_rect = self.level_image.get_rect()
